72870c352eb0ce01dbcdc1673103aa77db8a7f9a
ruffino.cyprien <ruffino.cyprien@gmail.com>
2014-02-18 16:26:43 +0100

    Encodeur et décodeur notes/entier
    	new file:   src/harmonie/Encoder.java
    	new file:   src/harmonie/EncoderTest.java

---
+++ src/harmonie/Encoder.java
@@ -0,0 +1,41 @@
+package harmonie;
+
+public class Encoder {
+	public static int encoder(int[] notes) {
+		int encode = 0;
+		for (int i = 0; i < 8; i = i + 2) {
+			encode <<= 5;
+			encode += notes[i];
+			encode <<= 2;
+			encode += notes[i + 1];
+		}
+		encode <<= 4;
+		encode += notes[8];
+		return encode;
+	}
+
+	public static int[] decode(int code) {
+		int[] notes = new int[9];
+		notes[8] = getIntFromNBits(code, 4);
+		code >>>= 4;
+		for (int i = 7; i >= 0; i = i - 2) {
+			notes[i] = getIntFromNBits(code, 2);
+			code >>>= 2;
+			notes[i - 1] = getIntFromNBits(code, 5);
+			code >>>= 5;
+		}
+		return notes;
+	}
+
+	private static int getBit(int n, int k) {
+		return (n >> k) & 1;
+	}
+
+	private static int getIntFromNBits(int num, int e) {
+		int res = 0;
+		for (int i = 0; i < e; i++) {
+			res += getBit(num, i) * Math.pow(2, i);
+		}
+		return res;
+	}
+}
---
+++ src/harmonie/EncoderTest.java
@@ -0,0 +1,17 @@
+package harmonie;
+
+import static org.junit.Assert.*;
+import java.math.*;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+public class EncoderTest {
+	@Test
+	public void test() {
+		int[] notes = { 24, 1, 24, 1, 24, 1, 24, 1, 4 };
+		int test = Encoder.encoder(notes);
+		assertArrayEquals(notes, Encoder.decode(test));
+	}
+}
