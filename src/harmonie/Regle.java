6258870f12e467d65bca891d365c4f60ed94ba09
Nicolas <nicolas.serrette@etu.univ-orleans.fr>
2014-02-20 08:46:10 +0100

    update regle

--- src/harmonie/Regle.java
+++ src/harmonie/Regle.java
@@ -3,23 +3,13 @@ package harmonie;
 public class Regle {
 
 	public boolean regle2(int[] tab) {
-		if (tab[6] < tab[4] && tab[4] < tab[2] && tab[2] < tab[0]) {// verifie
-																	// si les
-																	// differentes
-																	// note d'un
-																	// accord
-																	// sont bien
-																	// ordonée
-																	// B<T<A<S
+		if (tab[6] < tab[4] && tab[4] < tab[2] && tab[2] < tab[0]) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean regle1(int[] tab) {// verifife si les note de chaque
-										// table(soprano, alto, tenor, basse)
-										// sont
-										// dans le bon intervale
+	public boolean regle1(int[] tab) {
 		if (13 < tab[0] && tab[0] < 27 && 10 < tab[2] && tab[2] < 23
 				&& 6 < tab[4] && 2 < tab[6] && tab[6] < 16) {
 			return true;
@@ -27,13 +17,7 @@ public class Regle {
 		return false;
 	}
 
-	public boolean enchainement_Note_possible(int[] noteC, int[] noteP) {// verifie
-																			// si
-																			// deux
-		// note ce
-		// suivant sont
-		// de le bon
-		// interval +-6
+	public boolean enchainement_Note_possible(int[] noteC, int[] noteP) {
 		if ((noteP[0] - noteC[0]) < 7 && (noteC[0] - noteP[0]) < 7) {
 			if ((noteP[2] - noteC[2]) < 7 && (noteC[2] - noteP[2]) < 7) {
 				if ((noteP[4] - noteC[4]) < 7 && (noteC[4] - noteP[4]) < 7) {
@@ -46,8 +30,7 @@ public class Regle {
 		return false;
 	}
 
-	public boolean regle3(int[] note, int[] ac) {// verifie si l'accord est
-													// valide
+	public boolean regle3(int[] note, int[] ac) {
 		if ((note[6] % 7) == (ac[0] % 7)) {
 			if ((note[0] % 7) == (ac[1] % 7) || (note[2] % 7) == (ac[1] % 7)
 					|| (note[4] % 7) == (ac[1] % 7)) {
@@ -61,7 +44,7 @@ public class Regle {
 		return false;
 	}
 
-	public boolean regle4(int[] partition) {// regle 4
+	public boolean regle4(int[] partition) {
 		int[] ac = Encoder.decode(partition[0]);
 		int[] acf = Encoder.decode(partition[partition.length - 1]);
 		if (ac[8] != 4 || acf[8] != 4) {
@@ -70,7 +53,7 @@ public class Regle {
 		return false;
 	}
 
-	public boolean regle5(int AcC, int AcS) {//prend en parametre le num de l'accord courant et le num de l'accord suivant
+	public boolean regle5(int AcC, int AcS) {
 		switch (AcC) {
 		case (0):
 			if (AcS != 4) {
