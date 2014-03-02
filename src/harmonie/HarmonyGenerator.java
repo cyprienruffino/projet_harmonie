68e8528b90725c6f674d4d183549a7fc22503a20
ruffino.cyprien <ruffino.cyprien@gmail.com>
2014-03-02 15:39:29 +0100

    Génération des partitions avec enchaînement selon la norme du sujet

--- src/harmonie/HarmonyGenerator.java
+++ src/harmonie/HarmonyGenerator.java
@@ -2,51 +2,100 @@ package harmonie;
 
 import java.util.*;
 
-public class HarmonyGenerator { // /!\ Implémentation des enchaînements en cours!! Classe temporaire!!
+public class HarmonyGenerator {
 
-	private static ArrayList<Integer>[] jeu;
+	private static int[][] jeu;
 	private static ArrayList<Integer>[][] suivant;
-	
-	public static void generate(int[][] s) {
-		jeu = new ArrayList[s.length];
-		suivant=new ArrayList[s.length][];
-		for (int i = 0; i < jeu.length; i++)
-			jeu[i] = new ArrayList(); // Initialisation de la future partition
 
-		for (int i = 0; i < s.length; i++) {
-			for (int j = 0; j < 8; j++) { // Création de la liste de notes
-				if (Accords.isTierce(s[i][0], j)
-						|| Accords.isQuinte(s[i][0], j)) {
-					generateJeu(jeu,s[i][0], i, j, s[i][1]);
-				}
+	public static void generate(int[][] s) {
+		// Génère tous les cas possibles de partitions suivant le schéma du
+		// sujet. Utiliser getJeu et getSuivant pour le récupérer.
+		//
+		jeu = new int[s.length][16];
+		suivant = new ArrayList[s.length][];
+		generateJeu(s);
+		generateSuivant();
+	}
+
+	public int[][] getJeu() {
+		return jeu;
+	}
+
+	public ArrayList<Integer>[][] getSuivant() {
+		return suivant;
+	}
+
+	// A partir d'ici, tout est privé
+
+	private static void generateJeu(int[][] s) {
+		for (int i = 0; i < jeu.length; i++) {
+			int[][] temp = generateCombinaisons(s[i][0], i, s[i][8], s[i][1]);
+			for (int[] cas : temp) {
+				if (Regle.noteCorrect(cas))
+					addJeu(i, Encoder.encoder(cas));
 			}
 		}
 	}
-	public static int[][] generateUseablePartition(int[][]s){
-		generate(s);
-		int[][] temp=new int[s.length][9];
-		for(int i=0;i<s.length;i++){
-			temp[i]=Encoder.decode(s[i][0]);
-		}
-		return temp;
+
+	private static void generateSuivant() {
+		for (int i = 0; i < jeu.length; i++)
+			generateNext(jeu[i], i, getLast(jeu[i]));
 	}
 
-	private static void generateJeu(ArrayList<Integer>[] t, int note, int index, int accord, int duree) {
-		int[]temp=new int[9];
-		for(int[]cas:Accords.getCombinaisons(accord)){
-			temp[0]=note;
-			temp[1]=duree;
-			temp[2]=cas[0];	
-			temp[3]=duree;
-			temp[4]=cas[1];
-			temp[5]=duree;
-			temp[6]=Accords.getTonique(accord);
-			temp[7]=duree;
-			temp[8]=accord;
-			if(Regle.noteCorrect(cas)) t[index].add(Encoder.encoder(cas));
+	private static int[][] generateCombinaisons(int note, int index,
+			int accord, int duree) {
+		int[][] ret = new int[4][];
+		int[] temp = new int[9];
+		int i = 0;
+		for (int[] cas : Accords.getCombinaisons(accord)) {
+			temp[0] = note;
+			temp[1] = duree;
+			temp[2] = cas[0];
+			temp[3] = duree;
+			temp[4] = cas[1];
+			temp[5] = duree;
+			temp[6] = Accords.getTonique(accord);
+			temp[7] = duree;
+			temp[8] = accord;
+			ret[i] = temp;
+			i++;
+		}
+		return ret;
+	}
+
+	private static void generateNext(int[] noteC, int index, int pos) {
+		int[][] comb = generateCombinaisons(noteC[0], index, noteC[8], noteC[1]);
+		for (int[] noteS : comb)
+			if (appartiens(Encoder.encoder(noteS), jeu[index + 1])
+					&& Regle.enchainementCorrect(noteC, noteS))
+				suivant[index][pos].add(getIndice(Encoder.encoder(noteS),
+						jeu[index + 1]));
+	}
+
+	private static boolean appartiens(int t, int[] tab) {
+		return true;
+	}
+
+	private static int getIndice(int t, int[] tab) {
+		return 0;
+	}
+
+	private static void addJeu(int index, int note) {
+		if (getLast(jeu[index]) < jeu[index].length)
+			jeu[index][getLast(jeu[index])] = note;
+		else {
+			int[] temp = new int[jeu[index].length * 2];
+			for (int j = 0; j < jeu[index].length; j++) {
+				temp[j] = jeu[index][j];
+			}
+			jeu[index] = temp;
 		}
 	}
-	private static void generateNext(ArrayList<Integer>[] t, int note, int index, int accord, int duree){
-		
+
+	private static int getLast(int[] tab) {
+		int j = 0;
+		while (j < tab.length && tab[j] != 0)
+			j++;
+		return j;
 	}
 }
