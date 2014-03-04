f0c52cd212fa71fcbc5f034c8382c6b7fae08225
Nicolas <nicolas.serrette@etu.univ-orleans.fr>
2014-03-04 14:31:43 +0100

    Reuh

--- src/harmonie/MidiWriterTest.java
+++ src/harmonie/MidiWriterTest.java
@@ -9,8 +9,10 @@ public class MidiWriterTest {
 	@Test
 	public void test() {
 		int tab[]={0,1,12,1,14,1,27,1,3};
+		int tab3[]={4,1,0,1,12,1,25,1,3};
 		int e=Encoder.encoder(tab);
-		int tab2[]={e,e};
+		int e2=Encoder.encoder(tab3);
+		int tab2[]={e,e2};
 		try {
 			MidiWriter.encodeMidi(tab2,"test");
 		} catch (Exception e1) {
