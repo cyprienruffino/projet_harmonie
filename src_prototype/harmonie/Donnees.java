80789d0a178202fee046891b6beb744dc77821b5
ruffino.cyprien <ruffino.cyprien@gmail.com>
2014-03-14 18:10:00 +0100

    Refonte des dossiers

--- src_prototype/harmonie/Donnees.java
+++ src_prototype/harmonie/Donnees.java
@@ -1,6 +1,6 @@
 package harmonie;
 
-public class Accords {
+public class Donnees {
 	public static int[][] accords={{0,2,4},{1,3,5},{2,4,6},{3,5,0},{3,5,0},{4,6,1},{5,0,2},{6,1,3}};
 	
 	//la méthode nature de la classe regle fait la même chose que les trois methode suivante
