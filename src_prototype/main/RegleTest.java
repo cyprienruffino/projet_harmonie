95b10f256fb872251dc935b8e31e6e636d28245b
ruffino.cyprien <ruffino.cyprien@gmail.com>
2014-03-17 17:36:20 +0100

    JUnit

--- src_prototype/main/RegleTest.java
+++ src_prototype/main/RegleTest.java
@@ -13,7 +13,7 @@ import accords.*;
 public class RegleTest {
 
 	@Test
-	public void test() {
+	public void testCleanGen() {
 		Accord a1=new II(1,1,1,1,1);
 		Accord a2=new III(2,2,2,2,2);
 		Accord a3=new II(1,1,1,1,3);
@@ -28,6 +28,19 @@ public class RegleTest {
 		Assert.assertEquals(1,accords.size());
 		
 		Assert.assertEquals(3,accords.get(0).getDuree());
+	}
+	
+	@Test
+	public void testInitAccordsPossibles(){
+		ArrayList<Accord> liste=Regle.initAccordPossible(22);
+		Assert.assertEquals(liste.size(),3);
+	}
+	
+	@Test
+	public void testGenBasse(){
+		ArrayList<Accord> liste=Regle.initAccordPossible(22);
+		Assert.assertEquals(6,Regle.GenBasse(liste));
+		
 	}
 
 }
