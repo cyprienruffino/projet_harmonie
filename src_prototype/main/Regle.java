7c666e673d9cc21502dd6352bdc75762352c8b6f
Nicolas <nicolas.serrette@etu.univ-orleans.fr>
2014-03-17 16:46:49 +0100

    Ajout de la génération des accords possible dans la classe régle manque la génération du tenor

--- src_prototype/main/Regle.java
+++ src_prototype/main/Regle.java
@@ -24,15 +24,19 @@ public class Regle {
 	}
 
 	public static boolean regle2(Accord ac) {
-		if (ac.getBasse() < ac.getTenor() && ac.getTenor() < ac.getAlto() && ac.getAlto() < ac.getSoprano()) {
+		if (ac.getBasse() < ac.getTenor() && ac.getTenor() < ac.getAlto()
+				&& ac.getAlto() < ac.getSoprano()) {
 			return true;
 		}
 		return false;
 	}
 
-	public static boolean regle1(Accord ac) {// faire une variante soprano pour corentin
-		if ((13 < ac.getSoprano() && ac.getSoprano() < 27) && (10 < ac.getAlto() && ac.getAlto() < 23
-				) && (6 < ac.getTenor() && ac.getTenor()<20 )&& (2 < ac.getBasse() && ac.getBasse() < 16)) {
+	public static boolean regle1(Accord ac) {// faire une variante soprano pour
+												// corentin
+		if ((13 < ac.getSoprano() && ac.getSoprano() < 27)
+				&& (10 < ac.getAlto() && ac.getAlto() < 23)
+				&& (6 < ac.getTenor() && ac.getTenor() < 20)
+				&& (2 < ac.getBasse() && ac.getBasse() < 16)) {
 			return true;
 		}
 		return false;
@@ -80,49 +84,57 @@ public class Regle {
 	}
 
 	public static boolean regle3(Accord ac) {
-		int s=ac.getSoprano()%7;
-		int a=ac.getAlto()%7;
-		int t=ac.getTenor()%7;
-		int b=ac.getBasse()%7;
-		if(ac.getClass().equals(I.class)){
-			if(b==0){
-				if((s==2||s==4) && (a==2||a==4) && (t==2||t==4)){
+		int s = ac.getSoprano() % 7;
+		int a = ac.getAlto() % 7;
+		int t = ac.getTenor() % 7;
+		int b = ac.getBasse() % 7;
+		if (ac.getClass().equals(I.class)) {
+			if (b == 0) {
+				if ((s == 2 || s == 4) && (a == 2 || a == 4)
+						&& (t == 2 || t == 4)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(II.class)){
-			if(b==1){
-				if((s==3 || s==5)&&(a==3||a==5)&&(t==3||t==5)){
+		} else if (ac.getClass().equals(II.class)) {
+			if (b == 1) {
+				if ((s == 3 || s == 5) && (a == 3 || a == 5)
+						&& (t == 3 || t == 5)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(III.class)){
-			if(b==2){
-				if((s==4||s==6)&&(a==4||a==6)&&(t==4||t==6)){
+		} else if (ac.getClass().equals(III.class)) {
+			if (b == 2) {
+				if ((s == 4 || s == 6) && (a == 4 || a == 6)
+						&& (t == 4 || t == 6)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(IV.class)||ac.getClass().equals(IVb.class)){
-			if(b==3){
-				if((s==5||s==0)&&(a==5||a==0)&&(t==5||t==0)){
+		} else if (ac.getClass().equals(IV.class)
+				|| ac.getClass().equals(IVb.class)) {
+			if (b == 3) {
+				if ((s == 5 || s == 0) && (a == 5 || a == 0)
+						&& (t == 5 || t == 0)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(V.class)){
-			if(b==4){
-				if((s==6||s==1)&&(a==6||a==1)&&(t==6||t==1)){
+		} else if (ac.getClass().equals(V.class)) {
+			if (b == 4) {
+				if ((s == 6 || s == 1) && (a == 6 || a == 1)
+						&& (t == 6 || t == 1)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(VI.class)){
-			if(b==5){
-				if((s==0||s==2)&&(a==0||a==2)&&(t==0||t==2)){
+		} else if (ac.getClass().equals(VI.class)) {
+			if (b == 5) {
+				if ((s == 0 || s == 2) && (a == 0 || a == 2)
+						&& (t == 0 || t == 2)) {
 					return true;
 				}
 			}
-		}else if(ac.getClass().equals(VII.class)){
-			if(b==7){
-				if((s==1||s==3)&&(a==1||a==3)&&(t==1||t==3)){
+		} else if (ac.getClass().equals(VII.class)) {
+			if (b == 7) {
+				if ((s == 1 || s == 3) && (a == 1 || a == 3)
+						&& (t == 1 || t == 3)) {
 					return true;
 				}
 			}
@@ -131,7 +143,8 @@ public class Regle {
 	}
 
 	public static boolean regle4(Accord[] part) {
-		if (part[0].getClass().equals(IVb.class) || part[part.length-1].getClass().equals(IVb.class)) {
+		if (part[0].getClass().equals(IVb.class)
+				|| part[part.length - 1].getClass().equals(IVb.class)) {
 			return false;
 		}
 		return true;
@@ -153,11 +166,11 @@ public class Regle {
 				return true;
 			}
 		} else if (AcC.getClass().equals(IV.class)) {
-			if (!(AcS.getClass().equals(IVb.class))){
+			if (!(AcS.getClass().equals(IVb.class))) {
 				return true;
 			}
 		} else if (AcC.getClass().equals(IVb.class)) {
-			if (AcS.getClass().equals(I.class)){
+			if (AcS.getClass().equals(I.class)) {
 				return true;
 			}
 		} else if (AcC.getClass().equals(V.class)) {
@@ -268,41 +281,131 @@ public class Regle {
 		return -1;
 	}
 
-public static int accordPossible (int s){
-	s=s%7;
-	switch(s){
-	case(0):
-		return 460;
-	case(1):
-		return 157;
-	case(2):
-		return 206;
-	case(3):
-		return 147;
-	case(4):
-		return 520;
-	case(5):
-		return 146;
-	case(6):
-		return 257;
+	public static int nomAccord(Accord ac) {
+		if (ac.getClass().equals(I.class)) {
+			return 1;
+		} else if (ac.getClass().equals(II.class)) {
+			return 2;
+		} else if (ac.getClass().equals(III.class)) {
+			return 3;
+		} else if (ac.getClass().equals(IV.class)
+				|| ac.getClass().equals(IVb.class)) {
+			return 4;
+		} else if (ac.getClass().equals(V.class)) {
+			return 5;
+		} else if (ac.getClass().equals(VI.class)) {
+			return 6;
+		} else if (ac.getClass().equals(VII.class)) {
+			return 7;
+		}
+		return -1;
 	}
-	return-1;
-}
 
-public static ArrayList<Accord> generateCombinaison(int s){
-	ArrayList<Accord> poss=new ArrayList<Accord>();
-	switch(s%7){
-	case(0):
-		poss.add(new I(s,-1,-1,-1,-1));
-		poss.add(new VI(s,-1,-1,-1,-1));
-		poss.add(new IV(s,-1,-1,-1,-1));
-		poss.add(new IVb(s,-1,-1,-1,-1));
-		break;
-	case(1):
-		poss.add(new II(s,-1,-1,-1,-1));
-		poss.add(new V(s,-1,-1,-1,-1));
-		poss.add(new VII(s,-1,-1,-1,-1));
-		break;
+	public static ArrayList<Accord> initAccordPossible(int s) {
+		ArrayList<Accord> poss = new ArrayList<Accord>();
+		switch (s % 7) {
+		case (0):
+			poss.add(new I(s, -1, -1, -1, -1));
+			poss.add(new VI(s, -1, -1, -1, -1));
+			poss.add(new IV(s, -1, -1, -1, -1));
+			poss.add(new IVb(s, -1, -1, -1, -1));
+			break;
+		case (1):
+			poss.add(new II(s, -1, -1, -1, -1));
+			poss.add(new V(s, -1, -1, -1, -1));
+			poss.add(new VII(s, -1, -1, -1, -1));
+			break;
+		case (2):
+			poss.add(new III(s, -1, -1, -1, -1));
+			poss.add(new I(s, -1, -1, -1, -1));
+			poss.add(new VI(s, -1, -1, -1, -1));
+			break;
+		case (3):
+			poss.add(new II(s, -1, -1, -1, -1));
+			poss.add(new IV(s, -1, -1, -1, -1));
+			poss.add(new IVb(s, -1, -1, -1, -1));
+			poss.add(new VII(s, -1, -1, -1, -1));
+			break;
+		case (4):
+			poss.add(new I(s, -1, -1, -1, -1));
+			poss.add(new III(s, -1, -1, -1, -1));
+			poss.add(new V(s, -1, -1, -1, -1));
+			break;
+		case (5):
+			poss.add(new II(s, -1, -1, -1, -1));
+			poss.add(new IV(s, -1, -1, -1, -1));
+			poss.add(new IVb(s, -1, -1, -1, -1));
+			poss.add(new VI(s, -1, -1, -1, -1));
+			break;
+		case (6):
+			poss.add(new III(s, -1, -1, -1, -1));
+			poss.add(new V(s, -1, -1, -1, -1));
+			poss.add(new VII(s, -1, -1, -1, -1));
+			break;
+		}
+		return poss;
 	}
-}
+
+	public static void cleanGen(ArrayList<Accord> poss) {
+		Iterator<Accord> it = poss.iterator();
+		while (it.hasNext() && !(poss.get(0).getClass().equals(it.next()))) {
+			it.next();
+			it.remove();
+		}
+	}
+
+	public static void GenBasse(ArrayList<Accord> poss) {
+		Iterator<Accord> it = poss.iterator();
+		while (it.hasNext() && !(poss.get(0).getClass().equals(it.next()))) {
+			Accord ac = it.next();
+			int a = nomAccord(ac);
+			for (int i = 3; i < 16; i++) {
+				if (i % 7 == a) {
+					ac.setBasse(i);
+					poss.add(ac);
+				}
+			}
+		}
+	}
+
+	public static void GenAlto(ArrayList<Accord> poss) {
+		Iterator<Accord> it = poss.iterator();
+		while (it.hasNext() && !(poss.get(0).getClass().equals(it.next()))) {
+			Accord ac = it.next();
+			int a = nomAccord(ac);
+			for (int i = 11; i < Math.max(22, ac.getSoprano()); i++) {
+				if (i > ac.getBasse()) {
+					if (ac.getBasse() % 7 == ac.getSoprano() % 7) {
+						if (i % 7 == (a + 2) % 7 || i % 7 == (a + 4) % 7) {
+							ac.setAlto(i);
+							poss.add(ac);
+						}
+					} else if (i % 7 == a || i % 7 == (a + 2) % 7
+							|| i % 7 == (a + 4) % 7) {
+						ac.setAlto(i);
+						poss.add(ac);
+					}
+				}
+			}
+		}
+	}
+
+	public static ArrayList<Accord> generateCombinaison(int s) {
+		ArrayList<Accord> poss = new ArrayList<Accord>();
+		poss = initAccordPossible(s);
+		GenBasse(poss);
+		cleanGen(poss);
+		GenAlto(poss);
+		cleanGen(poss);
+		
+		cleanGen(poss);
+		Iterator<Accord> it = poss.iterator();
+		Accord ac;
+		while(it.hasNext()){
+			ac=it.next();
+			if(!noteCorrect(ac))
+				it.remove();
+		}
+		return poss;
+	}
 }
